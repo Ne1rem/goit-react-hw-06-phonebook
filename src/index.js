9e2e7089d759b6b0eb295e82b8929324a3370eb9
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-09-14 13:30:00 +0300

    s

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// export const theme = {
+//   colors: {
+//     black: '#212121',
+//     white: '#fff',
+//     success: 'green',
+//     warning: 'orange',
+//     error: 'red',
+//   },
+// };
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
